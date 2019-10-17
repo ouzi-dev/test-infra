ee2b9f303325ec20dc72bde491c697d07291e665
Alex Ouzounis <alex@ouzi.dev>
2019-10-17 14:11:52 +0100

    Feat/switch to prow tf module (#21)
    * Switch to prow cluster tf module
    
    * Use the prow cluster module

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -10,98 +10,8 @@ variable "gcloud_project" {
   description = "Name of the GKE project"
 }
 
-variable "system" {
-  description = "The system name"
-  default     = "testinfra"
-}
-
-# See: https://cloud.google.com/kubernetes-engine/docs/how-to/ip-aliases
-variable "gke_node_cidr_range" {
-  description = "VPC nodes CIDR range"
-}
-
-variable "gke_pod_cidr_range" {
-  description = "VPC pods CIDR range"
-}
-
-variable "gke_service_cidr_range" {
-  description = "VPC services CIDR range"
-}
-
-variable "gke_master_cidr_range" {
-  description = "CIDR range for masters"
-}
-
-variable "gke_node_scopes" {
-  description = "The GKE node scopes"
-  type        = list(string)
-  default = [
-    "https://www.googleapis.com/auth/compute",
-    "https://www.googleapis.com/auth/devstorage.read_write",
-    "https://www.googleapis.com/auth/logging.write",
-    "https://www.googleapis.com/auth/monitoring",
-  ]
-}
-
-variable "gke_auth_cidr_blocks" {
-  type        = list
-  description = "Authorized cidr blocks for the API"
-}
-
 variable "gke_kubernetes_version" {
   description = "Minimum k8s master version"
-}
-
-variable "gke_machine_type" {
-  description = "Instance type for the primary pool of workers"
-}
-
-variable "gke_big_machine_type" {
-  description = "Instance type for the beefier pool of workers"
-}
-
-variable "gke_machine_disk_size" {
-  description = "Disk size for the primary pool of workers"
-}
-
-variable "gke_machine_is_preemptible" {
-  description = "If true use preemptible instances"
-}
-
-variable "gke_min_nodes" {
-  description = "Min number of workers"
-  default     = 0
-}
-
-variable "gke_max_nodes" {
-  description = "Max number of workers"
-}
-
-variable "gke_daily_maintenance" {
-  default = "02:00"
-}
-
-variable "gke_disable_hpa" {
-  default = false
-}
-
-variable "gke_disable_lb" {
-  default = false
-}
-
-variable "gke_disable_dashboard" {
-  default = false
-}
-
-variable "gke_disable_network_policy" {
-  default = false
-}
-
-variable "gke_enable_calico" {
-  default = true
-}
-
-variable "gke_init_nodes" {
 }
 
 variable "gke_authenticator_groups_security_group" {
@@ -146,7 +56,6 @@ variable "slack_bot_token_credstash_key" {
 variable "dockerconfig_credstash_key" {
   type = string
 }
-
 
 variable "credstash_region" {
   type = string
