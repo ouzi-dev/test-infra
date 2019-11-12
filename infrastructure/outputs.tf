006ccaf4dede95c7176a167c2697f7aca1a862a2
Alex Ouzounis <alex@ouzi.dev>
2019-11-12 16:12:54 +0000

    feat: use credstash to store secrets from tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,19 +1,40 @@
-# GCloud outputs
-output "gcloud_region" {
-  value = var.gcloud_region
+output "valuesyaml" {
+  value     = module.prow-cluster.valuesyaml
+  sensitive = true
 }
 
 output "gcloud_project" {
   value = var.gcloud_project
 }
 
-# GKE outputs
+output "gcloud_region" {
+  value = var.gcloud_region
+}
 
 output "gke_name" {
   value = module.prow-cluster.gke_name
 }
 
-output "valuesyaml" {
-  value     = module.prow-cluster.valuesyaml
+output "prow_bucket_name" {
+  value = module.prow-cluster.prow_artefacts_bucket_name
+}
+
+output "prow_bucket_svc_account_key" {
+  value = module.prow-cluster.prow_bucket_svc_account_key
+  sensitive = true
+}
+
+output "certmanager_svc_account_key" {
+  value = module.prow-cluster.certmanager_svc_account_key
+  sensitive = true
+}
+
+output "prow_aws_access_key_id" {
+  value = module.prow-cluster.prow_terraform_aws_svc_account_access_key_id
+  sensitive = true
+}
+
+output "prow_aws_secret_access_key" {
+  value = module.prow-cluster.prow_terraform_aws_svc_account_secret_access_key
   sensitive = true
 }
